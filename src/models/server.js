6440b88c5168e9003242ece2587e014c1cfe7afc
DevJose <devjose@email.com>
2024-06-02 17:20:51 -0600

    fix: cors

--- src/models/server.js
+++ src/models/server.js
@@ -16,7 +16,13 @@ export class ServerModel {
 
 
     middlewares = () => {
-        this.app.use(cors());
+        this.app.use(cors(
+            {
+                origin: "http://localhost:5173/",
+                methods: ["GET", "POST", "PUT", "DELETE"],
+                allowedHeaders: ["Content-Type", "Authorization"],
+            }
+        ));
     }
 
     routes = () => {
